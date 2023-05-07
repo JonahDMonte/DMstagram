d64491bd3a6adf15b3078645625f278d0735cf31
Jonah D'Monte <jonahd146@gmail.com>
2023-05-06 23:05:56 -0400

    works

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -7,49 +9,61 @@ from printsleep import printsleeptime
 import threading
 import typing
 import functools
+import asyncio
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-token = "Njk3MTY0OTQ2MDkzMzc1NTE4.GM5J5Q.lMiBeSiQ20IW0iY2VFpR2olM0rgWQxTc9uxIYw"
-stopflag = False
+token = "Njk3MTY0OTQ2MDkzMzc1NTE4.GQEivi.zTGPvHv590JUSno24yFXtD6jlxKZdpoNY_p6B4"
+
 
 @bot.event
 async def on_ready():
     print(f'Bot connected as {bot.user}')
 
+async def startup(message):
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.listening, name='Instagram DMs'))
+    instamsgs = wi.checkmsgs()
+    with open("saved.json", "r") as f:
+        saved = json.load(f)
+    try:
+        if instamsgs != saved and instamsgs != []:
+            with open("saved.json", "w") as f:
+                json.dump(instamsgs, f)
+
+            embed = discord.Embed(title='Unread Messages', color=0xfc00bf)
+
+
+            embed.set_author(name='DMstagram',
+                             icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png',
+                             url='https://www.instagram.com/direct/inbox/')
+
+            for i in instamsgs:
+                embed.add_field(name=i[0], value=i[1], inline=False)
+
+            await message.channel.send(embed=embed)
+        await bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.listening, name='Pink Floyd'))
+        print("check successful")
+    except Exception as e:
+        print("Check Failed. See Error:" )
+        print(e)
+    await asyncio.sleep(60)
 
 @bot.event
 async def on_message(message):
+
+    stopflag = False
     if message.content == 'test':
         await message.channel.send('Testing 1 2 3')
-@bot.event
-async def on_message(message):
-    if message.content == 'stop':
-        await message.channel.send("Stopping thread")
-        stopflag = True
 
-
-async def startup(message):
-        instamsgs = wi.checkmsgs()
-        print(instamsgs)
-        embed = discord.Embed(title='Unread Messages', color=0xfc00bf)
-        print("building embed")
-
-        embed.set_author(name='DMstagram',
-                         icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png',
-                         url='https://www.instagram.com/direct/inbox/')
-
-        for i in instamsgs:
-            embed.add_field(name=i[0], value=i[1], inline=False)
-
-        await message.channel.send(embed=embed)
-        printsleeptime(899, 901)
-
-@bot.event
-async def on_message(message):
     if message.content == 'startup':
         await message.channel.send("Starting up!")
         while not stopflag:
             await startup(message)
+
+    if message.content == 'stop':
+        await message.channel.send("Stopping thread")
+        stopflag = True
 
 bot.run(token)
 
--- webinteractions.py
+++ webinteractions.py
@@ -11,7 +11,7 @@ accounts = {
   "instagramUsername": "6476738000",
   "instagramPassword": "Globie123",
   "autologin": True,
-  "mutedusers": ["Ahnenerbe Cafe", "OG Gangsters"],
+  "mutedusers": ["OG Gangsters", "Ahnenerbe Cafe"],
   "mutereels": True,
 }
 
@@ -26,7 +26,6 @@ mutedusers = accounts['mutedusers']
 
 def login():
     actions = ActionChains(driver)
-    print("logging in: step 1/3\r", end='', flush=True)
     actions.pause(3)  # required for instagram to load properly
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.TAB)
@@ -36,7 +35,6 @@ def login():
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.ENTER)
-    print("logging in: step 2/3\r", end='', flush=True)
     actions.pause(5)  # required for instagram to load properly
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.TAB)
@@ -45,16 +43,15 @@ def login():
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.ENTER)
-    print("logging in: step 3/3\r", end='', flush=True)
     actions.pause(2)  # required for instagram to load properly
     actions.send_keys(Keys.TAB)
     actions.send_keys(Keys.ENTER)
     actions.perform()
-    print("logged in\r", end='', flush=True)
+
 
 
 def checkmsgs():
-    print("checking messages")
+
     new_msg_span_class = "_aacl _aaco _aacw _aacx _aad7"
     new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
     driver.refresh()
@@ -63,7 +60,7 @@ def checkmsgs():
     spans_arr = driver.find_elements(By.TAG_NAME, "span")
     users = []
     msgs = []
-    print("sorting spans")
+
     for i in spans_arr:
         if i.get_attribute("class") == new_msg_user_span_class:
             users.append(i.text)
@@ -73,11 +70,14 @@ def checkmsgs():
 
     filtered = []
     n = 0
-    print("filtering")
+
     while n < len(users):
-        filtered.append([users[n], msgs[n]])
+        if not (users[n] in accounts["mutedusers"] or (msgs[n] == "Sent you a message" and accounts["mutereels"] == True) or (msgs[n] == "Sent a post " and accounts["mutereels"] == True)):
+            filtered.append([users[n], msgs[n]])
+
         n += 1
-    print("sending")
+
+
     return filtered
 
 def exitweb():
