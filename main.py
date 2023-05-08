2c4ea18ff696929e57f40e6fee43123d0c30b564
Jonah D'Monte <jonahd146@gmail.com>
2023-05-08 15:31:23 -0400

    added responding

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import asyncio
 import json
 
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
 import webinteractions as wi
-from discord.ext import tasks
-from printsleep import printsleeptime
-import threading
-import typing
-import functools
-import asyncio
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 token = "Njk3MTY0OTQ2MDkzMzc1NTE4.GQEivi.zTGPvHv590JUSno24yFXtD6jlxKZdpoNY_p6B4"
-
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+ser = Service(r"C:\Users\Jonah D'Monte\Documents\DMstagram\chromedriver.exe")
+driver = webdriver.Chrome(service=ser, options=chrome_options)
+webi = wi.webinteractor(driver)
+webi.login()
 
 @bot.event
 async def on_ready():
@@ -22,48 +29,60 @@ async def on_ready():
 async def startup(message):
     await bot.change_presence(
         activity=discord.Activity(type=discord.ActivityType.listening, name='Instagram DMs'))
-    instamsgs = wi.checkmsgs()
+
+    instamsgs = webi.checkmsgs()
     with open("saved.json", "r") as f:
         saved = json.load(f)
-    try:
-        if instamsgs != saved and instamsgs != []:
-            with open("saved.json", "w") as f:
-                json.dump(instamsgs, f)
+    # try:
+    if instamsgs != saved and instamsgs != []:
+        with open("saved.json", "w") as f:
+            json.dump(instamsgs, f)
 
-            embed = discord.Embed(title='Unread Messages', color=0xfc00bf)
+        embed = discord.Embed(title='Unread Messages', color=0xfc00bf)
 
 
-            embed.set_author(name='DMstagram',
-                             icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png',
-                             url='https://www.instagram.com/direct/inbox/')
+        embed.set_author(name='DMstagram',
+                         icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png',
+                         url='https://www.instagram.com/direct/inbox/')
 
-            for i in instamsgs:
-                embed.add_field(name=i[0], value=i[1], inline=False)
+        for i in instamsgs:
+            embed.add_field(name=i[0], value=i[1], inline=False)
 
-            await message.channel.send(embed=embed)
-        await bot.change_presence(
-            activity=discord.Activity(type=discord.ActivityType.listening, name='Pink Floyd'))
-        print("check successful")
-    except Exception as e:
-        print("Check Failed. See Error:" )
-        print(e)
-    await asyncio.sleep(60)
+        await message.channel.send(embed=embed)
+
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.listening, name='Pink Floyd'))
+
+    print("Check Successful\r", end='', flush=True)
+    # except:
+    #     message.channel.send("Check failed.")
+    #
+    #     print("Check Failed. Attempting relogin.")
+    #     await bot.change_presence(
+    #         activity = discord.Activity(type=discord.ActivityType.listening, name='Troubleshooting'))
+    #     webi.relogin()
+
 
 @bot.event
 async def on_message(message):
-
-    stopflag = False
     if message.content == 'test':
         await message.channel.send('Testing 1 2 3')
 
     if message.content == 'startup':
         await message.channel.send("Starting up!")
-        while not stopflag:
-            await startup(message)
 
-    if message.content == 'stop':
-        await message.channel.send("Stopping thread")
-        stopflag = True
+        while True:
+            await startup(message)
+            await asyncio.sleep(60)
+
+    if message.content.startswith("respond"):
+        await message.channel.send("Responding")
+        str = message.content.split(',,')
+        await webi.respond(str[1], str[2])
+        await message.channel.send("Response sent")
+
+    if message.content == 'relogin':
+        await message.channel.send(webi.relogin())
 
 bot.run(token)
 
--- webinteractions.py
+++ webinteractions.py
@@ -7,82 +7,114 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-accounts = {
-  "instagramUsername": "6476738000",
-  "instagramPassword": "Globie123",
-  "autologin": True,
-  "mutedusers": ["OG Gangsters", "Ahnenerbe Cafe"],
-  "mutereels": True,
-}
+class webinteractor:
+    def __init__(self, driver):
 
-chrome_options = Options()
-chrome_options.add_argument("--headless=new")  # remove this line to debug browser
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.instagram.com/direct/inbox/")
-username = accounts['instagramUsername']
-password = accounts['instagramPassword']
-mutedusers = accounts['mutedusers']
+        self.accounts = {
+          "instagramUsername": "6476738000",
+          "instagramPassword": "Globie123",
+          "autologin": True,
+          "mutedusers": ["OG Gangsters", "Ahnenerbe Cafe"],
+          "mutereels": False,
+        }
 
-
-def login():
-    actions = ActionChains(driver)
-    actions.pause(3)  # required for instagram to load properly
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(username)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(password)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.ENTER)
-    actions.pause(5)  # required for instagram to load properly
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.ENTER)
-    actions.pause(2)  # required for instagram to load properly
-    actions.send_keys(Keys.TAB)
-    actions.send_keys(Keys.ENTER)
-    actions.perform()
+          # remove this line to debug browser
+        self.driver = driver
+        self.driver.get("https://www.instagram.com/direct/inbox/")
+        self.username = self.accounts['instagramUsername']
+        self.password = self.accounts['instagramPassword']
+        self.mutedusers = self.accounts['mutedusers']
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless=new")
 
 
+    def login(self):
+        self.actions = ActionChains(self.driver)
+        self.actions.pause(3)  # required for instagram to load properly
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(self.username)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(self.password)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.ENTER)
+        self.actions.pause(5)  # required for instagram to load properly
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.ENTER)
+        self.actions.pause(2)  # required for instagram to load properly
+        self.actions.send_keys(Keys.TAB)
+        self.actions.send_keys(Keys.ENTER)
+        self.actions.perform()
 
-def checkmsgs():
+    def autologin(self):
+        if self.accounts["autologin"]:
+            self.login()
 
-    new_msg_span_class = "_aacl _aaco _aacw _aacx _aad7"
-    new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
-    driver.refresh()
-    time.sleep(6)  # required for instagram to load properly
+    def checkmsgs(self):
+        new_msg_span_class = "_aacl _aaco _aacw _aacx _aad7"
+        new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
+        self.driver.refresh()
+        time.sleep(6)  # required for instagram to load properly
+        users = []
+        msgs = []
+        spans_arr = self.driver.find_elements(By.TAG_NAME, "span")
+        try:
+            users = [i.text for i in spans_arr if i.get_attribute('class') == new_msg_user_span_class]
+            msgs = [i.text for i in spans_arr if i.get_attribute('class') == new_msg_span_class]
+        except:
+            self.relogin()
+            return [['Checkmsgs error occurred', 'Relogin attempted']]
 
-    spans_arr = driver.find_elements(By.TAG_NAME, "span")
-    users = []
-    msgs = []
+        filtered = []
+        n = 0
+        while n < len(users):
+            if not (users[n] in self.accounts["mutedusers"]):
+                # or (msgs[n] == "Sent you a message" and accounts["mutereels"] == True) or (msgs[n] == "Sent a post " and accounts["mutereels"] == True)
+                if msgs[n] == "Sent you a message":
+                    filtered.append([users[n], "Reel"])
+                elif msgs[n] == "Sent a post":
+                    filtered.append([users[n], "Post"])
+                else:
+                    filtered.append([users[n], msgs[n]])
+            n += 1
+        return filtered
 
-    for i in spans_arr:
-        if i.get_attribute("class") == new_msg_user_span_class:
-            users.append(i.text)
+    def respond(self, user, msg):
+        new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
+        self.driver.refresh()
+        time.sleep(6)
+        spans_arr = self.driver.find_elements(By.TAG_NAME, "span")
+        try:
+            users = [i for i in spans_arr if i.get_attribute('class') == new_msg_user_span_class]
+        except:
+            self.relogin()
+            return [['Respond error occurred', 'Relogin attempted']]
 
-        if i.get_attribute("class") == new_msg_span_class:
-            msgs.append(i.text)
+        for i in users:
+            if i.text == user:
+                i.click()
+                time.sleep(3)
+                sender = ActionChains(self.driver)
+                sender.send_keys(msg)
+                sender.send_keys(Keys.ENTER)
+                sender.perform()
+                self.driver.get("https://www.instagram.com/direct/inbox/")
+            else:
+                pass
 
-    filtered = []
-    n = 0
-
-    while n < len(users):
-        if not (users[n] in accounts["mutedusers"] or (msgs[n] == "Sent you a message" and accounts["mutereels"] == True) or (msgs[n] == "Sent a post " and accounts["mutereels"] == True)):
-            filtered.append([users[n], msgs[n]])
-
-        n += 1
 
 
-    return filtered
-
-def exitweb():
-    driver.quit()
-
-if accounts["autologin"]:
-    login()
+    def relogin(self):
+        print("Activating relogin")
+        self.driver.quit()
+        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.driver.get("https://www.instagram.com/direct/inbox/")
+        self.login()
+        return 'Relogin complete'
 
