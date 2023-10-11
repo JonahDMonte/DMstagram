22e4242b45ead7b19e727eaa3adbe45222b8b5af
Jonah D'Monte <jonahd146@gmail.com>
2023-10-11 18:10:49 -0400

    Removed sensitive information

--- DMstagram.py
+++ DMstagram.py
@@ -13,7 +13,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+# x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv xuk3077 x1oa3qoh x1nhvcw1
+# x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv xuk3077 x1oa3qoh x1nhvcw1
+# _aacl _aaco _aacu _aacx _aad6 _aade
 class dmstagram:
     def __init__(self, username, password, mutedusers):
 
@@ -25,15 +31,22 @@ class dmstagram:
           "mutereels": False,
         }
 
-
+        self.new_msg_span_class = "x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft"
+        #
+        self.new_msg_user_span_class = "x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft"
+        #
+        self.read_msg_span_class = "_aacl _aaco _aacu _aacy _aad7"
+        self.read_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp xo1l8bm x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
         self.chrome_options = Options()
-        self.chrome_options.add_argument("--headless=new")
+        #self.chrome_options.add_argument("--headless=new")
         self.ser = Service(r"C:\Users\Jonah D'Monte\Documents\DMstagram\chromedriver.exe")
         self.driver = webdriver.Chrome(service=self.ser, options=self.chrome_options)
+        self.driver.implicitly_wait(10)
         self.driver.get("https://www.instagram.com/direct/inbox/")
         self.username = self.accounts['instagramUsername']
         self.password = self.accounts['instagramPassword']
         self.mutedusers = self.accounts['mutedusers']
+        self.msg_spans_arr = []
 
 
 
@@ -60,36 +73,66 @@ class dmstagram:
         self.actions.send_keys(Keys.TAB)
         self.actions.send_keys(Keys.ENTER)
         self.actions.perform()
+        print(self.driver.current_url)
 
     def autologin(self):
         if self.accounts["autologin"]:
             self.login()
+    def refresh_span_tags(self):
+        if self.driver.current_url ==  "https://www.instagram.com/direct/inbox/":
+            self.driver.refresh()
+        else:
+            self.driver.get("https://www.instagram.com/direct/inbox/")
+        time.sleep(10)
+
+        refresh_users = [i for i in self.driver.find_elements(By.TAG_NAME, "span") if i.text == "Millia Rage"]
+        refresh_msgs = [i for i in self.driver.find_elements(By.TAG_NAME, "span") if i.text == "test"]
+
+        print(refresh_users)
+        print(refresh_msgs)
+        self.new_msg_user_span_class = refresh_users[0].get_attribute("class")
+        self.new_msg_span_class = refresh_msgs[0].get_attribute("class")
+
+    def refresh_msg_spans_arr(self):
+        new_msg_span_class = "_aacl _aaco _aacw _aacx _aad7"
+        new_msg_span_class = "x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft"
+        new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
+        new_msg_user_span_class = "x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft"
+        read_msg_span_class = "_aacl _aaco _aacu _aacy _aad7"
+        read_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp xo1l8bm x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
+        self.refresh_span_tags()
+        span_filters = [self.new_msg_span_class, self.new_msg_user_span_class, self.read_msg_span_class, self.read_msg_user_span_class]
+
+        self.msg_spans_arr = [i for i in self.driver.find_elements(By.TAG_NAME, "span") if i.get_attribute("class") in span_filters]
+
 
     def msgcheck(self):
-        new_msg_span_class = "_aacl _aaco _aacw _aacx _aad7"
-        new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
-        self.driver.refresh()
-        time.sleep(6)  # required for instagram to load properly
+        self.refresh_msg_spans_arr()
         users = []
         msgs = []
-        spans_arr = self.driver.find_elements(By.TAG_NAME, "span")
+
         try:
-            users = [i.text for i in spans_arr if i.get_attribute('class') == new_msg_user_span_class]
-            msgs = [i.text for i in spans_arr if i.get_attribute('class') == new_msg_span_class]
+            users = [i.text for i in self.msg_spans_arr if i.get_attribute('class') == self.new_msg_user_span_class]
+            msgs = [i.text for i in  self.msg_spans_arr if i.get_attribute('class') == self.new_msg_span_class]
         except:
             self.relogin()
             return [['Checkmsgs error occurred', 'Relogin attempted']]
 
         filtered = []
         n = 0
+        print("users")
+        print(users)
+        print("msgs")
+        print(msgs)
+
         while n < len(users):
             try:
                 if not (users[n] in self.accounts["mutedusers"]):
                     # or (msgs[n] == "Sent you a message" and accounts["mutereels"] == True) or (msgs[n] == "Sent a post " and accounts["mutereels"] == True)
                     if msgs[n] == "Sent you a message":
-                        filtered.append([users[n], "Reel"])
+                        filtered.append([users[n], "Sent you a reel"]) # todo implement reel downloading
                     elif msgs[n] == "Sent a post":
-                        filtered.append([users[n], "Post"])
+                        filtered.append([users[n], "Sent you a post"]) # todo implement post embedding
                     else:
                         filtered.append([users[n], msgs[n]])
             except:
@@ -98,13 +141,11 @@ class dmstagram:
             n += 1
         return filtered
 
-    def respond(self, user, msg):
-        new_msg_user_span_class = "x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs xt0psk2 x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"
-        self.driver.refresh()
-        time.sleep(6)
-        spans_arr = self.driver.find_elements(By.TAG_NAME, "span")
+    def respond(self, user, msg, attachments=None):
+        self.refresh_msg_spans_arr()
+
         try:
-            users = [i for i in spans_arr if i.get_attribute('class') == new_msg_user_span_class]
+            users = [i for i in self.msg_spans_arr if i.get_attribute('class') == self.new_msg_user_span_class or i.get_attribute('class') == self.read_msg_user_span_class]
         except:
             self.relogin()
             return 'Respond error occurred, relogin attempted'
@@ -114,9 +155,14 @@ class dmstagram:
                 try:
                     i.click()
                     time.sleep(3)
+                    print(self.driver.page_source)
+                    print("\n\n\n\n")
                     sender = ActionChains(self.driver)
                     sender.send_keys(msg)
                     sender.send_keys(Keys.ENTER)
+                    print(self.driver.page_source)
+                    if attachments:
+                        pass # todo implement attachment sending
                     sender.perform()
                     self.driver.get("https://www.instagram.com/direct/inbox/")
                     return "Response sent!"
@@ -135,4 +181,5 @@ class dmstagram:
         self.driver.get("https://www.instagram.com/direct/inbox/")
         self.login()
         return 'Relogin complete'
+
 
--- main.py
+++ main.py
@@ -3,24 +3,66 @@ import json
 
 import discord
 from discord.ext import commands
+from discord import guild
 
 
 import DMstagram as dm
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-token = "Njk3MTY0OTQ2MDkzMzc1NTE4.GQEivi.zTGPvHv590JUSno24yFXtD6jlxKZdpoNY_p6B4"
 
-webi = dm.dmstagram("6476738000", "Globie123", ["OG Gangsters", "Ahnenerbe Cafe"],)
+with open("sensitive_info.json", "r") as f:
+    sensitiveinfo = json.load(f)
+
+accountinfo = sensitiveinfo[0]
+token = accountinfo['discordtoken']
+
+webi = dm.dmstagram(accountinfo['instagramUsername'], accountinfo["instagramPassword"], accountinfo['mutedusers'])
 webi.login()
+insta_to_discord = sensitiveinfo[1]
+
+discord_to_insta = {v:k for k, v in insta_to_discord.items()}
+
+
+
+
 @bot.event
 async def on_ready():
     print(f'Bot connected as {bot.user}')
 
+
+async def send_response(user, text):
+    webi.respond(user, text)
+
+
+@bot.command()
+async def create_channel(ctx, channel_name): #todo fix duplicate channel creation
+    guild = ctx.guild
+    category = discord.utils.get(guild.categories, name="Instagram")
+    channels = [i.name for i in bot.get_all_channels()]
+    channels = channels[channels.index('Instagram') + 1:]
+
+    print(channels)
+
+    # replace "Instagram" with the name of your desired category
+
+    if not (insta_to_discord[channel_name] in channels):
+        print(f'Creating a new channel: {channel_name}')
+        n = await guild.create_text_channel(channel_name, category=category)
+        return n
+    else:
+        n = discord.utils.get(guild.channels, name=insta_to_discord[channel_name])
+        print(n)
+        return(n)
+        print(f'A channel with the name "{channel_name}" already exists in the category "{category.name}".')
+
+
+
+
 async def startup(message, n):
     await bot.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.listening, name='Instagram DMs'))
-
+        activity=discord.Activity(type=discord.ActivityType.listening, name='Reading DMs'))
     instamsgs = webi.msgcheck()
+
     with open("saved.json", "r") as f:
         saved = json.load(f)
     # try:
@@ -28,57 +70,54 @@ async def startup(message, n):
         with open("saved.json", "w") as f:
             json.dump(instamsgs, f)
 
-        embed = discord.Embed(title='Unread Messages', color=0xfc00bf)
-
-
-        embed.set_author(name='DMstagram',
-                         icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png',
-                         url='https://www.instagram.com/direct/inbox/')
-
+        print("instamsgs:")
+        print(instamsgs)
         for i in instamsgs:
-            embed.add_field(name=i[0], value=i[1], inline=False)
-
-        await message.channel.send(embed=embed)
+            c = await create_channel(message, i[0])
+            await c.send(i[1])
 
     await bot.change_presence(
         activity=discord.Activity(type=discord.ActivityType.listening, name='Pink Floyd'))
-
     print(f"Checks: {n}\r", end='', flush=True)
-    # except:
-    #     message.channel.send("Check failed.")
-    #
-    #     print("Check Failed. Attempting relogin.")
-    #     await bot.change_presence(
-    #         activity = discord.Activity(type=discord.ActivityType.listening, name='Troubleshooting'))
-    #     webi.relogin()
 
+async def refresh():
+    webi.refresh_span_tags()
 
-async def send_response(x, y, msg):
-    await msg.channel.send(webi.respond(x, y))
 
 @bot.event
 async def on_message(message):
-    if message.content == 'test':
+    if message.content == '!test':
         await message.channel.send('Testing 1 2 3')
 
-    if message.content == 'startup':
-
+    if message.content == '!startup':
         n = 0
         await message.channel.send("Starting up!")
 
         while True:
             await startup(message, n)
             n += 1
-            await asyncio.sleep(60)
+            await asyncio.sleep(5)
 
     if message.content.startswith("respond"):
         await message.channel.send("Responding")
         str = message.content.split(',,')
-        await send_response(str[1], str[2], message)
+        await send_response(str[1], str[2])
 
-
-    if message.content == 'relogin':
+    if message.content == '!relogin':
         await message.channel.send(webi.relogin())
+    if message.content == '!channel':
+        await create_channel(message, 'test')
+    if message.content == "!refresh":
+        await refresh()
+
+
+    if message.channel.category == discord.utils.get(message.guild.channels, name="Instagram") and not(message.author == bot.user):
+        await bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.listening, name='Sending DMs'))
+        await send_response(discord_to_insta[message.channel.name], message.content)
+        await bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.listening, name='Patrick Bruel'))
+
 
 bot.run(token)
 
